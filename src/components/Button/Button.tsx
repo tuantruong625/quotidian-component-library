4f05b982353a26706c78b660af65863d8a3d17a0
Tuan N. Truong <tuan.truong625@gmail.com>
2022-03-14 23:18:22 -0400

    Base styles for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import styled from "styled-components";
 export interface ButtonProps {
   label: string;
 }
 
-const Button = ({label}: ButtonProps) => {
-  return <button>{label}</button>;
+const QuotidianButton = styled.button`
+  background-color: #2E3440;
+  padding: 0.625rem 0.5rem;
+  border-radius: 5px;
+`
+
+const Button = ({ label, ...props }: ButtonProps) => {
+  return <QuotidianButton {...props}>{label}</QuotidianButton>;
 };
 
 export default Button;
