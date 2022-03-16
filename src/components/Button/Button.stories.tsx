c2eb466df0aeda8f8b0029422e130be7b11f1031
Tuan N. Truong <tuan.truong625@gmail.com>
2022-03-16 16:26:08 -0400

    Add storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+ title: 'QuotidianComponentLibrary/Button',
+ component: Button,
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+ label: 'Primary button',
+};
+
+export const ClickMe = Template.bind({});
+ClickMe.args = {
+ label: 'Click Me',
+};
