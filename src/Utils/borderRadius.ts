63957131951b93936f177f6c613b9f6ff7c47f09
Tuan N. Truong <tuan.truong625@gmail.com>
2022-03-28 16:41:27 -0400

    Add scaling units

---
+++ src/Utils/borderRadius.ts
@@ -0,0 +1,19 @@
+export const none = '0px'
+export const sm = '2px'
+export const md = '6px'
+export const lg = '8px'
+export const xl = '12px'
+export const xxl = '16px'
+export const xxxl = '24px'
+export const full = '9999px'
+
+export default {
+ none,
+ sm,
+ md,
+ lg,
+ xl,
+ xxl,
+ xxxl,
+ full,
+}
