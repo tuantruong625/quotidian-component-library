c3c8bb0191a879675d7789b341fa7540577a603f
Tuan N. Truong <tuan.truong625@gmail.com>
2022-04-26 17:12:20 -0400

    WIP Heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const Heading = (): JSX.Element => {
+type HeadingProps = {
+ type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+}
+
+const Heading = (type: HeadingProps): JSX.Element => {
  return (
   <h1>Heading</h1>
  )
