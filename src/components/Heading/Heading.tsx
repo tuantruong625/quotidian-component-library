a3bced4b0a527399d840a48cfbb8a2ce32405158
Tuan N. Truong <tuan.truong625@gmail.com>
2022-04-26 23:03:24 -0400

    Add heading component

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { createElement } from 'react'
+import styled, { createGlobalStyle, css } from 'styled-components'
+import { Colors, Spacing, Typography } from '../../utils'
 
-type HeadingProps = {
- type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+export interface HeadingProps {
+ type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5',
+ children: React.ReactNode
+}
+interface StyleProps {
+ type: string
 }
 
-const Heading = (type: HeadingProps): JSX.Element => {
+const QuotidianHeader = styled.h1<StyleProps>`
+ margin: ${Spacing.size5} 0;
+ line-height: ${Spacing.size7};
+ color: ${Colors.gray6};
+
+ ${({ type }) => type === 'h1' && css`
+  font-size: ${Typography.heading1};
+ `} 
+  
+ ${({ type }) => type === 'h2' && css`
+  font-size: ${Typography.heading2};
+ `}
+
+ ${({ type }) => type === 'h2' && css`
+  font-size: ${Typography.heading2};
+ `}
+
+ ${({ type }) => type === 'h3' && css`
+  font-size: ${Typography.heading3};
+ `}
+
+ ${({ type }) => type === 'h4' && css`
+  font-size: ${Typography.heading4};
+ `}
+`
+
+const Heading = ({ type = 'h1', children, ...props }: HeadingProps): JSX.Element => {
  return (
-  <h1>Heading</h1>
+  <QuotidianHeader as={type} type={type}>{children}</QuotidianHeader>
  )
 }
 
