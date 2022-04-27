a47161bcadea72cfba5e6a11d3bb8b737abf39c9
Tuan N. Truong <tuan.truong625@gmail.com>
2022-04-26 23:02:37 -0400

    Add size 7

--- src/Utils/spacing.ts
+++ src/Utils/spacing.ts
@@ -1,10 +1,11 @@
-export const size1 = '0.25rem'   
-export const size2 = '0.5rem'  
-export const size3 = '0.75rem'     
-export const size4 = '1rem'   
-export const size5 = '1.5rem'     
-export const size6 = '2rem'     
-export const size7 = '2.5rem'     
+export const size1 = '0.25rem'
+export const size2 = '0.5rem'
+export const size3 = '0.75rem'
+export const size4 = '1rem'
+export const size5 = '1.5rem'
+export const size6 = '2rem'
+export const size7 = '2.5rem'
+export const size8 = '3rem'
 
 export default {
  size1,
