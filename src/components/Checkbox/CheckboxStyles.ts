46ca87956182e49b9810b0671fe3fe05905e7084
Tuan N. Truong <tuan.truong625@gmail.com>
2022-06-01 16:30:19 -0400

    Refactor types and export styles

---
+++ src/components/Checkbox/CheckboxStyles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+import { BorderRadius, Colors, Spacing } from "../../utils"
+
+type StyleProps = {
+ checkmarkIcon: boolean
+}
+
+export const QuotidianLabel = styled.label`
+ display: flex;
+ align-items: center;
+ color: ${Colors.gray4};
+`
+export const QuotidianCheckbox = styled.input<StyleProps>`
+ margin-right: ${Spacing.size1};
+ -webkit-appearance: none;
+ appearance: none;
+ color: ${Colors.gray6};
+ border-radius: ${BorderRadius.sm};
+ height: ${Spacing.size4};
+ width: ${Spacing.size4};
+ border: 1px solid ${Colors.gray6};
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ cursor: pointer;
+ 
+
+ &::before {
+  content: "";
+  height: 10px;
+  width: 10px;
+  ${({ checkmarkIcon }) => checkmarkIcon && `clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);`}
+  transform: scale(0);
+  transform-origin: bottom left;
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset 1em 1em ${Colors.primary3};
+  background-color: ${Colors.primary3};
+  border-radius: ${BorderRadius.sm};
+ }
+
+ &:checked::before {
+  transform: scale(1);
+ }
+`
