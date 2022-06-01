47dd5cf7d51e9b80eebcf76866326b3565483135
Tuan N. Truong <tuan.truong625@gmail.com>
2022-06-01 16:28:42 -0400

    Refactor button types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,18 @@
-import React, { ReactElement, useLayoutEffect, useRef, useState } from "react";
+import React, { HTMLProps, ReactElement, useLayoutEffect, useRef, useState } from "react";
 import { IconProps, QuotidianButton } from "./ButtonStyles";
 import Loader from "../Loader";
 import './Button.css'
 
-export type ButtonProps = {
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   label?: string,
   disabled?: boolean,
   shape?: "sm" | "md" | "lg" | "full",
   variant?: "primary" | "outline" | "text",
   loader?: boolean,
   icon?: ReactElement<IconProps>,
-  onClick?: () => void
 }
 
-const Button = ({ label, shape, variant = 'primary', disabled, loader, onClick, icon, ...props }: ButtonProps): JSX.Element => {
+const Button = ({ label, shape, variant = 'primary', disabled, loader, icon, ...props }: ButtonProps): JSX.Element => {
   const buttonRef = useRef<HTMLButtonElement>(null)
   const [buttonWidth, setButtonWidth] = useState<number>()
   const [buttonHeight, setButtonHeight] = useState<number>()
@@ -30,7 +29,6 @@ const Button = ({ label, shape, variant = 'primary', disabled, loader, onClick, 
       shape={shape}
       variant={variant}
       disabled={disabled || loader}
-      onClick={onClick}
       loader={loader}
       height={buttonHeight}
       width={buttonWidth}
