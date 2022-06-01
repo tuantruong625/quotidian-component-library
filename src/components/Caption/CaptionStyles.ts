ee41b8e7f35e6b0d62f360c899175ab267c8d8fe
Tuan N. Truong <tuan.truong625@gmail.com>
2022-06-01 16:29:40 -0400

    Refactor types and export styles

---
+++ src/components/Caption/CaptionStyles.ts
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components'
+import { Colors, Spacing, Typography } from '../../utils'
+
+type StyleProps = {
+ isCapitalized: boolean,
+ isUppercase: boolean
+}
+
+export const QuotidianCaption = styled.p<StyleProps>`
+font-size: ${Typography.caption};
+color: ${Colors.gray4};
+margin: ${Spacing.size1} 0;
+
+${({ isCapitalized, isUppercase }) => isCapitalized && !isUppercase && css`
+ &::first-letter {
+  text-transform: capitalize;
+ }
+`}
+
+${({ isUppercase, isCapitalized }) => isUppercase && !isCapitalized && css`
+  text-transform: uppercase;
+  letter-spacing: ${Spacing.size1};
+`}
+`
