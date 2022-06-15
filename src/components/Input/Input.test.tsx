1e607eba67595ef9dc028bdc2d52a7610053496a
Tuan N. Truong <tuan.truong625@gmail.com>
2022-06-15 16:50:59 -0400

    Add tests

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { UserIcon } from "@heroicons/react/outline";
+import Input from "./Input";
+
+const renderComponent = (props = {}) => {
+ render(
+  <Input
+   type="text"
+   placeholder="input with icon"
+   label="Input with icon"
+   {...props}
+  />
+ );
+};
+describe("Input", () => {
+ it("renders an icon", () => {
+  renderComponent({ icon: <UserIcon /> });
+
+  // expect(screen.getByLabelText("Input with icon")).toMatchSnapshot()
+  expect(screen.getByLabelText("Input with icon")).toMatchSnapshot()
+ });
+});
